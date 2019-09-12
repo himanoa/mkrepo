a3eedee1f715dd778d92cea5e9572516e3a14abf
himanoa <matsunoappy@gmail.com>
2019-09-13 03:57:55 +0900

    Add version information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate clap;
+use clap::App;
+
+const VERSION: &'static str = "0.0.1";
 fn main() {
-    println!("Hello, world!");
+    App::new("Make project directory for ghq style.")
+        .version(VERSION)
+        .get_matches();
 }
