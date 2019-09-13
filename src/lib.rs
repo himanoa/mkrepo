8af9228a0326b1d90c51a0041ff109a6ab1bb254
himanoa <matsunoappy@gmail.com>
2019-09-13 16:03:19 +0900

    Implemented DummyExecutor

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+pub mod makerepo {
+    #[derive(Debug)]
+    pub enum CommandType<'a> {
+        CreateDirectory {
+            repository: &'a str,
+            author: &'a str,
+            hosting_service: &'a str,
+        },
+        InitializeGit {
+            first_commit_message: &'a str,
+        },
+    }
+
+    pub trait Executor {
+        fn execute(&self, commands: Vec<CommandType>) -> Result<(), ()>;
+    }
+
+    pub struct DryRunExecutor {}
+
+    impl DryRunExecutor {
+        pub fn new() -> DryRunExecutor {
+            DryRunExecutor {}
+        }
+    }
+
+    impl Executor for DryRunExecutor {
+        fn execute(&self, commands: Vec<CommandType>) -> Result<(), ()> {
+            for command in commands {
+                match command {
+                    CommandType::CreateDirectory {
+                        repository,
+                        author,
+                        hosting_service,
+                    } => println!("CreateDirectory: {}, {}, {}", repository, author, hosting_service),
+                    CommandType::InitializeGit {
+                        first_commit_message,
+                    } => println!("InitializeGit {}", first_commit_message),
+                }
+            }
+            Ok(())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg};
+mod lib;
+use lib::makerepo::{CommandType, DryRunExecutor, Executor};
 
 const VERSION: &'static str = "0.0.1";
 fn main() {
     App::new("Make project directory for ghq style.")
         .version(VERSION)
+        .arg(
+            Arg::with_name("project_name")
+                .help("Repository name")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
+    let executor = DryRunExecutor::new();
+    match executor.execute(vec![CommandType::CreateDirectory {
+        author: "himanoa",
+        repository: "hugo",
+        hosting_service: "foobar",
+    }, CommandType::InitializeGit { first_commit_message: "Initial commit" }]) {
+        Ok(()) => (),
+        Err(_) => ()
+    };
 }
